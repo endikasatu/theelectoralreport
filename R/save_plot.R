b271992a5bcbe058c6fe9a8b310bda2bed7e45cd
endikasatu <endika.nl@gmail.com>
2021-08-02 13:32:12 +0200

    Update several functions

---
+++ R/save_plot.R
@@ -0,0 +1,17 @@
+#' @title save_plot
+#'
+#' @description Esta función exporta los gráficos a un directorio.
+#'
+#' @import ggplot2
+#'
+#' @export
+
+ save_plot <- function (plot, width = 575, height = 500, save_path, plot_name, extension = ".pdf") {
+
+   ggplot2::ggsave(filename = paste0(save_path, plot_name, extension),
+                   plot = plot,
+                   width = (width/96),
+                   height = (height/96))
+ }
+
+
