8cdcf985fde48cdefa318dd38578625c65d1e9d4
endikasatu <endika.nl@gmail.com>
2021-08-03 10:14:38 +0200

    Fix save_plot

--- R/save_plot.R
+++ R/save_plot.R
@@ -6,9 +6,9 @@
 #'
 #' @export
 
- save_plot <- function (plot, width = 575, height = 500, save_path, plot_name, extension = ".pdf") {
+ save_plot <- function(plot, width = 575, height = 500, save_path, plot_name, extension = ".pdf") {
 
-   ggplot2::ggsave(filename = paste0(save_path, plot_name, extension),
+   ggplot2::ggsave(filename = paste0(save_path, "/", plot_name, extension),
                    plot = plot,
                    width = (width/96),
                    height = (height/96))
